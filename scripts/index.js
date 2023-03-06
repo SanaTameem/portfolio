97989f9e65316d571d41dd8cd97cf581c6baa2db
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-03-06 20:01:54 +0430

    Add more EventListeners to index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,14 @@
 const Menu = document.querySelector('section.mobile-menu-container');
-const MenuBtn = document.querySelector("a.hamburger");
+const MenuBtn = document.querySelector('a.hamburger');
 const CloseBtn = document.querySelector('span.menu-icon');
 
+MenuBtn.addEventListener('click', () => {
+  Menu.classList.add('visible');
+});
 
-MenuBtn.addEventListener('click', ()=>{
-  Menu.classList.add("visible");
-})
-
-CloseBtn.addEventListener('click', ()=>{
-  Menu.classList.remove("visible");
-})
+CloseBtn.addEventListener('click', () => {
+  Menu.classList.remove('visible');
+});
+Menu.addEventListener('click', () => {
+  Menu.classList.remove('visible');
+});
