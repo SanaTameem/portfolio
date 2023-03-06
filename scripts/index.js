0664454a640de5040e9e417349db13b88429d29a
Alvee <M.Alvee8141@gmail.com>
2023-03-06 21:03:28 +0600

    Added JS Click event
    Added JS Click event

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+const Menu = document.querySelector('section.mobile-menu-container');
+const MenuBtn = document.querySelector("a.hamburger");
+const CloseBtn = document.querySelector('span.menu-icon');
+
+
+MenuBtn.addEventListener('click', ()=>{
+  Menu.classList.add("visible");
+})
+
+CloseBtn.addEventListener('click', ()=>{
+  Menu.classList.remove("visible");
+})
